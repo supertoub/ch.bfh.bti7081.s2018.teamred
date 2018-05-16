00757a1322906ce25a072261963c2db411f6ef07
Roland <rolandroccaro@gmail.com>
2018-05-16 21:12:18 +0200

    LoginView V1

---
+++ app/src/main/java/UserInterface/LoginView.java
@@ -0,0 +1,37 @@
+package UserInterface;
+
+import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.declarative.Design;
+
+public class LoginView {
+    private VerticalLayout LoginViewLayout;
+    private Label usernameLabel;
+    private Label pwdLabel;
+    private Label returnMessageLabel;
+    private TextField usernameTextField;
+    private TextField pwdTextField;
+    private TextField returnMessageTextField;
+    private button LoginButton;
+    public Label getusernameLabel() {
+        return usernameLabel;
+    }
+    public Label getpwdLabel() {
+        return pwdLabel;
+    }
+    public Label getreturnMessageLabel() { return usernameLabel; }
+    public TextField getusernameTextField() { return usernameTextField; }
+    public TextField getpwdTextField() { return pwdTextField; }
+    public TextField getreturnMessageTextField() { return returnMessageTextField; }
+    public button getLoginButton() { return LoginButton; }
+
+    public LoginView() {
+        Design.read(this);
+    }
+    public VerticalLayout getLoginViewLayout() {
+        return LoginViewLayout;
+    }
+
+
+
+}
