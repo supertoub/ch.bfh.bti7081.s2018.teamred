bbdaa7a47902462931ee09091882875c8a75a771
Frido <ifridox@gmail.com>
2018-06-08 18:10:51 +0200

    removed unused imports

--- app/src/main/java/Data/PatientPersistence.java
+++ app/src/main/java/Data/PatientPersistence.java
@@ -1,7 +1,5 @@
 package Data;
 
-import Business.Level;
-import Business.LevelLibrary;
 import Business.Patient;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
