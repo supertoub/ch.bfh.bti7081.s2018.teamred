fe3acfd597aca53450a459004bb1c20e5bada456
Frido <ifridox@gmail.com>
2018-06-08 18:12:20 +0200

    Login is now done with users from persistence, removed unused imports

--- app/src/main/java/UserInterface/LoginView.java
+++ app/src/main/java/UserInterface/LoginView.java
@@ -4,15 +4,13 @@ import Business.ChallengeBoardPresenter;
 import Business.JournalLibraryPresenter;
 import Business.Patient;
 import Business.StartpagePresenter;
+import Data.PatientPersistence;
 import ch.bfh.MyUI;
-import com.vaadin.navigator.Navigator;
 import com.vaadin.navigator.View;
 import com.vaadin.server.VaadinSession;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Notification;
 import com.vaadin.ui.UI;
-
-import static ch.bfh.MyUI.CHALLENGEVIEW;
 
 public class LoginView extends LoginViewPage implements View {
 
@@ -24,59 +22,17 @@ public class LoginView extends LoginViewPage implements View {
     based on Login Example:
     source: https://examples.javacodegeeks.com/enterprise-java/vaadin/vaadin-login-example/
      */
-//
-//    @PersistenceContext
-//    private EntityManager entityManager;
-//    @Test
-//    public void genericDAOShouldSaveAChallengeEntity() throws Exception {
-//        // Save a person
-//        GenericDataFacade<Patient, String> patientDao = new GenericDataFacadeJPA<>(Patient.class);
-//
-//        patientDao.setEntityManager(entityManager);
-//        Patient patient = new Patient("Roccaro", "Roland","Pass",null,null,null);
-//        patient = patientDao.save(patient);
-//        Patient anotherPatient = patientDao.get(patient.getName());
-//    }
-
-    private String username = "RoccaroR";
-    private String password = "SavePW_1";
-
-    private void setUsername(String username) {
-        this.username = username;
-    }
-
-    private String getUsername(){
-        return this.username;
-    }
-
-
-    private void setPassword(String password) {
-        this.password = password;
-    }
-    private String getPassword(){
-        return this.password;
-    }
 
     public Boolean authenticate(String username, String password){
-        return username.equals(getUsername()) && password.equals(getPassword());
-    }
+        Patient patient = PatientPersistence.getInstance().getByName(username);
 
-
-/* TODO, reuse this code to do the login using DB
-    public Boolean authenticate(String username, String password){
-      GenericDataFacade<Patient, String> userDao = new GenericDataFacadeJPA<>(Patient.class);
-      String DBpassword = userDao.get(username).getPwd();
-
-       if(password.equals(DBpassword)){
-
-        //if(true){
-            return true;
-        }else{
+       if(password.equals(patient.getPwd())){
+           return true;
+        } else {
             return false;
-
         }
     }
-*/
+
     public void LoginbuttonClick(Button.ClickEvent event) {
             if(authenticate(usernameField.getValue(), passwordField.getValue())){
 
@@ -84,11 +40,11 @@ public class LoginView extends LoginViewPage implements View {
                 VaadinSession.getCurrent().setAttribute("user", usernameField.getValue());
 
                 StartpagePresenter startpagePresenter = StartpagePresenter.getInstance();
-                ChallengeBoardPresenter challangeBoardPresenter = ChallengeBoardPresenter.getInstance();
+                ChallengeBoardPresenter challengeBoardPresenter = ChallengeBoardPresenter.getInstance();
                 JournalLibraryPresenter journalPresenter = JournalLibraryPresenter.getInstance();
 
                 getUI().getNavigator().addView(MyUI.STARTPAGEVIEW, startpagePresenter.getStartView());
-                getUI().getNavigator().addView(MyUI.CHALLENGEVIEW, challangeBoardPresenter);
+                getUI().getNavigator().addView(MyUI.CHALLENGEVIEW, challengeBoardPresenter);
                 getUI().getNavigator().addView(MyUI.JOURNALVIEW, journalPresenter);
 
                 UI.getCurrent().getNavigator().navigateTo(MyUI.STARTPAGEVIEW);
