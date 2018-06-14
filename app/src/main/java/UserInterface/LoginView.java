7fc51ce68a3c1e6be07d65a72a55d85141977c12
Frido <ifridox@gmail.com>
2018-06-14 13:50:38 +0200

    set loginbutton as default button in loginview, added level 6 in intiial_data.sql

--- app/src/main/java/UserInterface/LoginView.java
+++ app/src/main/java/UserInterface/LoginView.java
@@ -6,6 +6,7 @@ import Business.Patient;
 import Business.StartpagePresenter;
 import Data.PatientPersistence;
 import ch.bfh.MyUI;
+import com.vaadin.event.ShortcutAction;
 import com.vaadin.navigator.View;
 import com.vaadin.server.VaadinSession;
 import com.vaadin.ui.Button;
@@ -16,6 +17,8 @@ public class LoginView extends LoginViewPage implements View {
 
     public LoginView () {
         super();
+        // set loginButton as default button
+        loginButton.setClickShortcut(ShortcutAction.KeyCode.ENTER);
         loginButton.addClickListener(this::LoginbuttonClick);
     }
         /*
