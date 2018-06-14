b7ba9a000c25b3cc6dd0504e2f1c97aaf7e8a7c3
Frido <ifridox@gmail.com>
2018-06-14 13:44:26 +0200

    removed unused import

--- app/src/main/java/Business/User.java
+++ app/src/main/java/Business/User.java
@@ -4,7 +4,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 import javax.persistence.Column;
-import static javax.persistence.GenerationType.AUTO;
 import static javax.persistence.GenerationType.IDENTITY;
 
 @MappedSuperclass
